4ff48a0f4fcce0f0a4ffc36fdfeda6e53305954b
Kamil <92475432+chrzanowskikamil@users.noreply.github.com>
2023-07-26 20:34:45 +0200

    feature: L01-51 react-bootstrap, yup and formik installed. (#1)

--- apps/shop/app/page.tsx
+++ apps/shop/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from './page.module.scss';
 import { Common } from '@lopi-2/common';
+import FormExample from '../components/form';
 
 export default async function Index() {
   return (
     <div className={styles.page}>
       <span>Hello World Shop: LOPI-2</span>
       <Common />
+      <FormExample />
     </div>
   );
 }
