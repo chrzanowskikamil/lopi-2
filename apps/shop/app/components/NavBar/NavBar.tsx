dc63212ac3a23e25fcba4bf465d20fffbfa59e83
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-08-18 13:24:20 +0200

    Megre.

--- apps/shop/app/components/NavBar/NavBar.tsx
+++ apps/shop/app/components/NavBar/NavBar.tsx
@@ -11,21 +11,9 @@ import { useState, FC } from 'react';
 import Slider from './components/Slider';
 import Socials from './components/Socials';
 
-import MenuDesktop from './components/MenuDesktop';
-import MenuMobile from './components/MenuMobile';
-import MenuDropdownMobile from './components/MenuDropdownMobile';
-
-import MenuDesktop from './components/MenuDesktop';
-import MenuMobile from './components/MenuMobile';
-import MenuDropdownMobile from './components/MenuDropdownMobile';
-
 import { Logo } from '../../assets/SvgIcons/Logo';
 
 const NavBar: FC = () => {
-  const [menuMobileActive, setMenuMobileActive] = useState(false);
-
-  const mobileFlag = () => setMenuMobileActive(!menuMobileActive);
-
   const [menuMobileActive, setMenuMobileActive] = useState(false);
 
   const mobileFlag = () => setMenuMobileActive(!menuMobileActive);
