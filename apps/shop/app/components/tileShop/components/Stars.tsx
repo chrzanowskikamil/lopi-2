792266fc3949b78d8f2717956765628de40b523a
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-08-18 14:34:02 +0200

    Added map instead of var.

--- apps/shop/app/components/tileShop/components/Stars.tsx
+++ apps/shop/app/components/tileShop/components/Stars.tsx
@@ -11,12 +11,8 @@ type StarsProps = {
 };
 
 const Stars: FC<StarsProps> = ({ starsCount }) => {
-  const fullStar = (
-    <StarIcon color={'var(--theme-orange)'} className={style.star} />
-  );
-  const emptyStar = (
-    <StarIcon color={'var(--theme-grey)'} className={style.star} />
-  );
+  const fullStar = <StarIcon className={style.star && style.starOrange} />;
+  const emptyStar = <StarIcon className={style.star} />;
   const ratingStar = Array.from({ length: 5 }, (elem, index) => {
     return (
       <span key={index}>{index <= starsCount - 1 ? fullStar : emptyStar}</span>
