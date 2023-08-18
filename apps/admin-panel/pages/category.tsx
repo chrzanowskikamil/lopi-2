c7d7950741d50e6df1c716abdf4b33e3e8243892
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-08-18 17:23:27 +0200

    L01-56 ADMIN dodawanie produktów. Before rebase.

--- apps/admin-panel/pages/category.tsx
+++ apps/admin-panel/pages/category.tsx
@@ -1,8 +1,8 @@
-'use client';
+import '../app/styles/admin-global.scss';
 
-import AddCategory from '../../../components/CategoryManagment/AddCategory/AddCategory';
-import EditCategoryChoice from '../../../components/CategoryManagment/EditCategory/EditCategoryChoice';
-import RemoveCategory from '../../../components/CategoryManagment/RemoveCategory/RemoveCategory';
+import AddCategory from '../components/CategoryManagment/AddCategory/AddCategory';
+import EditCategoryChoice from '../components/CategoryManagment/EditCategory/EditCategoryChoice';
+import RemoveCategory from '../components/CategoryManagment/RemoveCategory/RemoveCategory';
 
 import Accordion from 'react-bootstrap/Accordion';
 
@@ -11,19 +11,19 @@ const Categories: React.FC = () => {
     <>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Adding product area.</Accordion.Header>
+          <Accordion.Header>Adding category area.</Accordion.Header>
           <Accordion.Body>
             <AddCategory />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Editing product area.</Accordion.Header>
+          <Accordion.Header>Editing category area.</Accordion.Header>
           <Accordion.Body>
             <EditCategoryChoice />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>Removing product area.</Accordion.Header>
+          <Accordion.Header>Removing category area.</Accordion.Header>
           <Accordion.Body>
             <RemoveCategory />
           </Accordion.Body>
