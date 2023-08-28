04c47c7becba3c82d31db1e54db869a521338ccc
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-08-28 20:38:05 +0200

    Some changes.

--- apps/shop/app/components/Categories/components/Products/Products.tsx
+++ apps/shop/app/components/Categories/components/Products/Products.tsx
@@ -1,104 +1,144 @@
+'use client';
+
 import styles from './Products.module.scss';
 import { Container, Row } from 'react-bootstrap';
 import { FC } from 'react';
 import { ProductsResponse } from 'apps/shop/types/ProductsResponse';
 import ProductTile from './components/tileShop/productTile';
-import { products } from './components/tileShop/products';
-
+// import { products } from './components/tileShop/products';
+import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import { useSearchParams } from 'next/navigation';
+import { getProducts } from 'apps/shop/actions/getProducts';
 
 interface ProductsProps {
   products: ProductsResponse;
 }
+const getData = async () => {
+  const res = await getProducts();
+  const data = res;
 
-export const Products: FC<ProductsProps> = () => {
+  return data;
+};
+
+export const Products: FC<ProductsProps> = async () => {
+  const [isClient, setIsClient] = useState(false);
+  const products = getData();
+  console.log(products);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const searchParams = useSearchParams();
   const sorting = searchParams.get('sort');
   const filterPriceLow = searchParams.get('filterPriceLow');
   const filterPriceHight = searchParams.get('filterPriceHight');
-  const availiblity = searchParams.get('availibilty');
+  const availible = searchParams.get('availibilty');
 
-  const sortProductsFromCheap = products
-    .slice()
-    .sort(
-      (a, b) =>
-        (a.currentPrice !== undefined ? a.currentPrice : a.price) -
-        (b.currentPrice !== undefined ? b.currentPrice : b.price)
-    );
-
-  const sortProductsFromExpensive = products
-    .slice()
-    .sort(
-      (a, b) =>
-        (b.currentPrice !== undefined ? b.currentPrice : b.price) -
-        (a.currentPrice !== undefined ? a.currentPrice : a.price)
-    );
-
-  const sortProductsFromAToZ = products.slice().sort((a, b) => {
-    const fa = a.name.toLowerCase();
-    const fb = b.name.toLowerCase();
-    if (fa < fb) {
-      return -1;
-    }
-    if (fa > fb) {
-      return 1;
-    }
-
-    return 0;
-  });
-
-  const sortProductsFromZToA = products.slice().sort((a, b) => {
-    const fa = a.name.toLowerCase();
-    const fb = b.name.toLowerCase();
-    if (fa > fb) {
-      return -1;
-    }
-    if (fa < fb) {
-      return 1;
-    }
-
-    return 0;
-  });
-
-  const currentSerach = () => {
-    if (sorting === null) {
-      return products;
-    } else if (sorting === 'Cena rosnąca') {
-      return sortProductsFromCheap;
-    } else if (sorting === 'Cena malejąca') {
-      return sortProductsFromExpensive;
-    } else if (sorting === 'Alfabetycznie A do Z') {
-      return sortProductsFromAToZ;
-    } else if (sorting === 'Alfabetycznie Z do A') {
-      return sortProductsFromZToA;
-    } else return [];
-  };
-
-  const renderedProducts = currentSerach().map((product) => {
+  const renderedProducts = products.map((product) => {
     return (
       <ProductTile
-        key={product.id}
-        id={product.id}
-        rewievs={product.rewievs}
-        picture={product.picture}
+        key={product.sku}
+        sku={product.sku}
         name={product.name}
-        price={product.price}
-        currentPrice={product.currentPrice}
-        status={product.status}
-        stars={product.stars}
+        imageUrl={product.imageUrls}
+        regularPrice={product.regularPrice}
+        discountPrice={product.discountPrice}
       />
     );
   });
 
   return (
     <>
-      {sorting}
       <Container>
-        <Row className={styles.products}>{...renderedProducts}</Row>
+        {isClient ? (
+          <Row className={styles.products}>{/* { renderedProducts} */}</Row>
+        ) : (
+          <Row className={styles.products}>
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </Row>
+        )}
       </Container>
     </>
   );
 };
+
+// const sortBySearchParams = () => {
+//   table = getData;
+//   if (sorting !== null) {
+//     if (sorting === 'Cena rosnaca') {
+//       table.sort(
+//         (a, b) =>
+//           (a.currentPrice !== undefined ? a.currentPrice : a.price) -
+//           (b.currentPrice !== undefined ? b.currentPrice : b.price)
+//       );
+//     } else if (sorting === 'Cena malejaca') {
+//       table.sort(
+//         (a, b) =>
+//           (b.currentPrice !== undefined ? b.currentPrice : b.price) -
+//           (a.currentPrice !== undefined ? a.currentPrice : a.price)
+//       );
+//     } else if (sorting === 'Alfabetycznie A do Z') {
+//       table.sort((a, b) => {
+//         const fa = a.name.toLowerCase();
+//         const fb = b.name.toLowerCase();
+//         if (fa < fb) {
+//           return -1;
+//         }
+//         if (fa > fb) {
+//           return 1;
+//         }
+
+//         return 0;
+//       });
+//     } else if (sorting === 'Alfabetycznie Z do A') {
+//       table.sort((a, b) => {
+//         const fa = a.name.toLowerCase();
+//         const fb = b.name.toLowerCase();
+//         if (fa > fb) {
+//           return -1;
+//         }
+//         if (fa < fb) {
+//           return 1;
+//         }
+
+//         return 0;
+//       });
+//     }
+//   }
+//   if (filterPriceLow !== null) {
+//     table.forEach((el, index) => {
+//       if (
+//         (el.currentPrice !== undefined ? el.currentPrice : el.price) >=
+//         parseInt(filterPriceLow)
+//       ) {
+//         return;
+//       } else return table.splice(index, 1);
+//     });
+//   }
+//   if (filterPriceHight !== null) {
+//     table.forEach((el, index) => {
+//       if (
+//         (el.currentPrice !== undefined ? el.currentPrice : el.price) <=
+//         parseInt(filterPriceHight)
+//       ) {
+//         return;
+//       } else return table.splice(index, 1);
+//     });
+//   }
+//   if (availible !== null) {
+//     // if (availiblity === 'true'){}
+//     table.forEach((el, index) => {
+//       if (el.availiblity === true) {
+//         return;
+//       } else return table.splice(index, 1);
+//     });
+//   }
+
+//   return console.table(table), table;
+// };
 
 // 'Cena rosnąca',
 // 'Cena malejąca',
@@ -107,3 +147,58 @@ export const Products: FC<ProductsProps> = () => {
 // 'Od najnowszych do najstarszych',
 // 'Od najstarszych do najnowszych',
 // https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+// const sortProductsFromCheap = products
+//     .slice()
+//     .sort(
+//       (a, b) =>
+//         (a.currentPrice !== undefined ? a.currentPrice : a.price) -
+//         (b.currentPrice !== undefined ? b.currentPrice : b.price)
+//     );
+
+//   const sortProductsFromExpensive = products
+//     .slice()
+//     .sort(
+//       (a, b) =>
+//         (b.currentPrice !== undefined ? b.currentPrice : b.price) -
+//         (a.currentPrice !== undefined ? a.currentPrice : a.price)
+//     );
+
+//   const sortProductsFromAToZ = products.slice().sort((a, b) => {
+//     const fa = a.name.toLowerCase();
+//     const fb = b.name.toLowerCase();
+//     if (fa < fb) {
+//       return -1;
+//     }
+//     if (fa > fb) {
+//       return 1;
+//     }
+
+//     return 0;
+//   });
+
+//   const sortProductsFromZToA = products.slice().sort((a, b) => {
+//     const fa = a.name.toLowerCase();
+//     const fb = b.name.toLowerCase();
+//     if (fa > fb) {
+//       return -1;
+//     }
+//     if (fa < fb) {
+//       return 1;
+//     }
+
+//     return 0;
+//   });
+
+//   const currentSerach = () => {
+//     if (sorting === null) {
+//       return products;
+//     } else if (sorting === 'Cena rosnaca') {
+//       return sortProductsFromCheap;
+//     } else if (sorting === 'Cena malejaca') {
+//       return sortProductsFromExpensive;
+//     } else if (sorting === 'Alfabetycznie A do Z') {
+//       return sortProductsFromAToZ;
+//     } else if (sorting === 'Alfabetycznie Z do A') {
+//       return sortProductsFromZToA;
+//     } else return [];
+//   };
