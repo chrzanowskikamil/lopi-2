51e54b317ccf5f8ef6c2e707f5764f4bbba1bfa0
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-08-31 11:16:26 +0200

    Checkout.

--- apps/shop/app/components/Categories/components/Products/Products.tsx
+++ apps/shop/app/components/Categories/components/Products/Products.tsx
@@ -1,13 +1,10 @@
 import styles from './Products.module.scss';
 import { Container, Row } from 'react-bootstrap';
-import { Container, Row } from 'react-bootstrap';
+
 import { FC } from 'react';
 import { ProductsResponse } from '../../../../../types/ProductsResponse';
 import ProductTile from './components/tileShop/productTile';
 
-import { useState, useEffect } from 'react';
-import Spinner from 'react-bootstrap/Spinner';
-import { useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import { useSearchParams } from 'next/navigation';
@@ -74,71 +71,12 @@ export const Products: FC<ProductsProps> = ({ products }) => {
   };
 
   const renderedProducts = sortBySearchParams().map((product) => {
-export const Products: FC<ProductsProps> = ({ products }) => {
-  const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const searchParams = useSearchParams();
-
-  const filterPriceLow = searchParams.get('filterPriceLow');
-  const filterPriceHight = searchParams.get('filterPriceHight');
-  const availible = searchParams.get('availability');
-
-  const sortBySearchParams = () => {
-    const table = [...products.products];
-    console.table(table);
-
-    if (filterPriceHight !== null) {
-      for (let i = table.length - 1; i >= 0; i -= 1) {
-        if (
-          (table[i].discountPrice !== null
-            ? table[i].discountPrice
-            : table[i].regularPrice) >= parseInt(filterPriceHight)
-        ) {
-          table.splice(i, 1);
-        }
-      }
-    }
-
-    if (filterPriceLow !== null) {
-      for (let i = table.length - 1; i >= 0; i -= 1) {
-        if (
-          (table[i].discountPrice !== null
-            ? table[i].discountPrice
-            : table[i].regularPrice) < parseInt(filterPriceLow)
-        ) {
-          table.splice(i, 1);
-        }
-      }
-    }
-
-    if (availible !== null) {
-      for (let i = table.length - 1; i >= 0; i -= 1) {
-        if (availible === 'true') {
-          if (table[i].status !== 'ACTIVE') {
-            table.splice(i, 1);
-          }
-        }
-        if (availible === 'false') {
-          if (table[i].status === 'ACTIVE') {
-            table.splice(i, 1);
-          }
-        }
-      }
-    }
-
-    return console.table(table), table;
-  };
-
-  const renderedProducts = sortBySearchParams().map((product) => {
     return (
       <ProductTile
         key={product.uid}
         sku={product.sku}
         name={product.name}
-        imagesUrls={product.imagesUrls}
+        imageUrls={product.imageUrls}
         regularPrice={product.regularPrice}
         discountPrice={product.discountPrice}
       />
