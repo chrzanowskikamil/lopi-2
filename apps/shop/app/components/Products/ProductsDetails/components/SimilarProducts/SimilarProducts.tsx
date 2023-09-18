b82d7f2a4918a375b25340667b7489e2139a4290
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-09-18 23:03:58 +0200

    Some more changes.

--- apps/shop/app/components/Products/ProductsDetails/components/SimilarProducts/SimilarProducts.tsx
+++ apps/shop/app/components/Products/ProductsDetails/components/SimilarProducts/SimilarProducts.tsx
@@ -9,14 +9,16 @@ export const SimilarProducts: FC = async () => {
   const productTwo = await getProduct('143a761d-2015-48fb-96bf-b02e04752bd4');
   const productThree = await getProduct('b5fba84e-1729-4d99-8412-8421d44a2a85');
 
+  const productArray = [productOne, productTwo, productThree];
+
   return (
     <div className={style.similarProducts}>
       <span>Podobne produkty</span>
       <div className={style.container}>
         <Row className={style.products}>
-          <ProductTileCol product={productOne} col={3} />
-          <ProductTileCol product={productTwo} col={3} />
-          <ProductTileCol product={productThree} col={3} />
+          {productArray.map((el, index) => (
+            <ProductTileCol product={el} col={3} key={index} />
+          ))}
         </Row>
       </div>
     </div>
