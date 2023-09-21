b1a5e98d7aeee04a29e326c5d266b1cda295607c
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-09-21 13:24:29 +0200

    Added Promise.all

--- apps/shop/app/(pages)/productdetails/[productdetails]/page.tsx
+++ apps/shop/app/(pages)/productdetails/[productdetails]/page.tsx
@@ -8,11 +8,11 @@ const ProductDetailsPage = async ({
 }) => {
   const product = await getProduct(params.productdetails);
 
-  const productOne = await getProduct('b5fba84e-1729-4d99-8412-8421d44a2a85');
-  const productTwo = await getProduct('143a761d-2015-48fb-96bf-b02e04752bd4');
-  const productThree = await getProduct('b5fba84e-1729-4d99-8412-8421d44a2a85');
-
-  const productArray = [productOne, productTwo, productThree];
+  const productArray = await Promise.all([
+    getProduct('b5fba84e-1729-4d99-8412-8421d44a2a85'),
+    getProduct('143a761d-2015-48fb-96bf-b02e04752bd4'),
+    getProduct('b5fba84e-1729-4d99-8412-8421d44a2a85'),
+  ]);
 
   return (
     <>{<ProductsDetails product={product} productArray={productArray} />}</>
