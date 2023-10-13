d03a641fb24a0c7ac22c646fcb469bfce05be458
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-10-13 18:43:00 +0200

    Some changes.

--- apps/shop/app/components/MainPage/ForthSection/components/CustomersOpinions/CustomersOpinions.tsx
+++ apps/shop/app/components/MainPage/ForthSection/components/CustomersOpinions/CustomersOpinions.tsx
@@ -1,3 +1,23 @@
-export const CustomersOpinions = async () => {
-  return <>opinie</>;
+import { ClientOpinionsCarolusel } from './components/ClientOpinionsCarousel';
+import Image from 'next/image';
+import bgCoffeBeans from '../../../../../assets/PNG/forthsection/bg-coffee-beans.png';
+import style from './customersOpinions.module.scss';
+
+export const CustomersOpinions = () => {
+  return (
+    <section className={style.customersOpinions}>
+      <Image
+        src={bgCoffeBeans}
+        alt={'Bg picture. Small contour of coffee beans.'}
+        className={style.bgImage}
+      />
+      <div className={style.titleArea}>
+        <div className={style.title}>opinie o naszych kawach</div>
+        <div className={style.subtitle}>
+          zaufaj opiniom naszych wiernych klientów
+        </div>
+        <ClientOpinionsCarolusel />
+      </div>
+    </section>
+  );
 };
--- apps/shop/app/components/MainPage/ForthSection/components/OurCustomers/OurCustomers.tsx
+++ apps/shop/app/components/MainPage/ForthSection/components/OurCustomers/OurCustomers.tsx
@@ -1,17 +1,23 @@
+import { CustomersGallery } from './components/CustomersGallery';
+import { FC } from 'react';
 import Image from 'next/image';
 import bgCoffePlants from '../../../../../assets/PNG/forthsection/bg-coffee-plants.png';
 import style from './ourCustomers.module.scss';
 
-export const OurCustomers = async () => {
+export const OurCustomers: FC = () => {
   return (
-    <section>
+    <section className={style.ourCustomers}>
       <div className={style.bgPictureContainer}>
         <Image
           src={bgCoffePlants}
           alt={'Coffee plants.Background picture'}
           className={style.bgPicture}
         />
+        <div className={style.opacityBlock}>
+          <div className={style.title}>Nasi klienci</div>
+        </div>
       </div>
+      <CustomersGallery />
     </section>
   );
 };
