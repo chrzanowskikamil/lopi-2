4d3e2b1227d23ae577951144423b68d0e58fe8f4
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-10-13 10:41:36 +0200

    Cosmetic changes.

--- apps/shop/app/page.tsx
+++ apps/shop/app/page.tsx
@@ -1,22 +1,9 @@
-import { Common } from '@lopi-2/common';
-import SignInForm from './components/Auth/SignInForm/SignInForm';
-import SignUpForm from './components/Auth/SignUpForm/SignUpForm';
-import { TempComponent } from './components/TempComponent';
 import { ThirdSection } from './components/MainPage/ThirdSection/ThirdSection';
 
 const Index = async () => {
   return (
     <>
-      {/* <div className={``}>
-        <span>Hello World Shop: LOPI-2</span>
-        <Common />
-
-        <SignInForm />
-        <SignUpForm />
-
-        <TempComponent /> */}
       <ThirdSection />
-      {/* </div> */}
     </>
   );
 };
