f95f140d93d54bb22c8b7ea9a72b58b2db0d110f
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-10-31 14:22:06 +0100

    Added changes so there is no jumping text.

--- apps/shop/app/page.tsx
+++ apps/shop/app/page.tsx
@@ -2,15 +2,16 @@ import { FourthSection } from './components/MainPage/FourthSection/FourthSection
 import { HeroSection } from './components/MainPage/HeroSection/HeroSection';
 import { SecondSection } from './components/MainPage/SecondSection/SecondSection';
 import { ThirdSection } from './components/MainPage/ThirdSection/ThirdSection';
+import style from './index.module.scss';
 
 const Index = async () => {
   return (
-    <>
+    <main className={style.main}>
       <HeroSection />
       <SecondSection />
       <ThirdSection />
       <FourthSection />
-    </>
+    </main>
   );
 };
 export default Index;
