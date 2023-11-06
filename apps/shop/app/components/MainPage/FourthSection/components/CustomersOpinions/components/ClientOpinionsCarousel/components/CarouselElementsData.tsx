ed6b999b177ea22c349c2f4353338a6f56d7b08b
Bartosz Stpiczyński <barstpiczynski@gmail.com>
2023-11-06 21:49:49 +0100

    Some changes.

--- apps/shop/app/components/MainPage/FourthSection/components/CustomersOpinions/components/ClientOpinionsCarousel/components/CarouselElementsData.tsx
+++ apps/shop/app/components/MainPage/FourthSection/components/CustomersOpinions/components/ClientOpinionsCarousel/components/CarouselElementsData.tsx
@@ -22,9 +22,11 @@ export const carouselElementsData: CarouselElementsData[] = [
     starCount: 5,
     opinion: (
       <span>
-        &rdquo;Bean&nbsp;Nook&nbsp;to<br />prawdziwy&nbsp;zakątek&nbsp;dla
-        miłośników&nbsp;kawy. Ich&nbsp;ekspresy&nbsp;do&nbsp;kawy
-        są&nbsp;znakomite,<br />a wybór&nbsp;kaw&nbsp;mielonych mnie zachwycił.
+        &rdquo;Bean&nbsp;Nook&nbsp;to
+        <br />
+        prawdziwy&nbsp;zakątek&nbsp;dla miłośników&nbsp;kawy.
+        Ich&nbsp;ekspresy&nbsp;do&nbsp;kawy są&nbsp;znakomite,
+        <br />a wybór&nbsp;kaw&nbsp;mielonych mnie zachwycił.
         <br />
         Polecam wszystkim!&rdquo;
       </span>
